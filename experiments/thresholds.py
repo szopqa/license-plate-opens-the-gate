1468c69c6d2fb92a28ca4a4ea4e0d0d5508d6a15
szopaa <michalszopa@outlook.com>
2018-09-03 23:16:18 +0200

    Added script testing threshold methods

---
+++ experiments/thresholds.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage import data
+from skimage.filters import try_all_threshold
+
+img = imread('../input_images/car4.jpg', as_grey=True)
+
+fig, ax = try_all_threshold(img, figsize=(10, 8), verbose=False)
+plt.show()
