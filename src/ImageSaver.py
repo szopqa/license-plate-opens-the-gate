25e8484fc8d3b9382b66f7fe478b7492ea11dd30
szopaa <michalszopa@outlook.com>
2018-10-08 23:34:16 +0200

    Added images saver

---
+++ src/ImageSaver.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import os
+
+class ImageSaver():
+
+  imagesSaved = 0;
+
+  def __init__(self, savedImagesDir='../output_images'): 
+    self.__now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    self.__savedImagesDir = savedImagesDir
+
+    if not os.path.exists(savedImagesDir):
+      os.makedirs(savedImagesDir)
+
+  def save(self, image_matrix):
+    ImageSaver.imagesSaved += 1
+    image_name = f'{self.__savedImagesDir}/{self.__now}_{ImageSaver.imagesSaved}.png';
+
+    img = Image.fromarray(image_matrix, mode = 'L')
+    print(image_name)
+    img.save(image_name);
--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ from Reader import Reader
 from LicensePlateValidator import LicensePlateValidator 
 from CanonicalCorrelationAnalyzer import CanonicalCorrelationAnalyzer
 from ImageDisplay import ImageDisplay
+from ImageSaver import ImageSaver
 
 reader = Reader('../input_images/car2.jpg')
+saver = ImageSaver();
 binary_image = reader.get_binary_resized(1)
 
 cca = CanonicalCorrelationAnalyzer(binary_image, LicensePlateValidator(allowed_mistake = 0.2))
@@ -17,4 +19,6 @@ cca.find_plate_like_objects()
 print(cca.plate_like_objects)
 
 for plate_like_object in cca.plate_like_objects:
-  ImageDisplay.show_image(plate_like_object)
+  print(plate_like_object.shape)
+  # ImageDisplay.show_image(plate_like_object)
+  saver.save(plate_like_object)
