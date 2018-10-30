ae474e73d38d4ae3aec62d92d46e1793c2657247
szopaa <michalszopa@outlook.com>
2018-10-30 22:23:55 +0100

    Extended saver

--- src/ImageSaver.py
+++ src/ImageSaver.py
@@ -15,9 +15,30 @@ class ImageSaver():
     if not os.path.exists(savedImagesDir):
       os.makedirs(savedImagesDir)
 
-  def save(self, image_matrix):
-    ImageSaver.imagesSaved += 1
-    image_name = f'{self.__savedImagesDir}/{self.__now}_{ImageSaver.imagesSaved}.png';
+  def __matrix_to_image(self, image_matrix):
+    return Image.fromarray(image_matrix, mode = 'L')
 
-    img = Image.fromarray(image_matrix, mode = 'L')
-    img.save(image_name);
+  def __save(self, image, image_name):
+    image.save(image_name)
+
+  def __set_image_name(self):
+    ImageSaver.imagesSaved += 1
+    return f'{self.__savedImagesDir}/{self.__now}_{ImageSaver.imagesSaved}.png'
+
+  def save(self, image_matrix):
+    self.__save(self.__matrix_to_image(image_matrix), self.__set_image_name())
+
+  def save_resized(self, image_matrix, width, height):    
+    img = self.__matrix_to_image(image_matrix)
+    resized = img.resize((width, height), Image.ANTIALIAS)
+    self.__save(resized, self.__set_image_name())
+
+  def save_as_train_data(self, image_matrix, width, height):
+    print(image_matrix)
+    image_matrix[image_matrix == 255] = 1
+    image_matrix[image_matrix == 0] = 255
+    image_matrix[image_matrix == 1] = 0
+    print(image_matrix.reshape(1, -1))
+    img = self.__matrix_to_image(image_matrix)
+    resized = img.resize((width, height), Image.ANTIALIAS)
+    self.__save(resized, self.__set_image_name())
