875e958e9dd1f48674611fd0a33c7f34fd388563
szopaa <michalszopa@outlook.com>
2018-11-07 15:23:33 +0100

    Fixed test for character segmenation

--- src/CharSegmentationTests.py
+++ src/CharSegmentationTests.py
@@ -6,23 +6,23 @@ from Predictor import Predictor
 from ImageSaver import ImageSaver
 from ModelMapper import ModelMapper
 
-reader = Reader('../output_images/2018-10-17T19:26:16_2.png')
-# reader = Reader('../output_images/2018-10-17T19:25:53_1.png')
+reader = Reader('../output_images/2018-10-17T19:25:41_1.png')
+# reader = Reader('../output_images/2018-11-03T15:58:28_2.png')
 
 segmentator = CharactersSegmentator(CharactersValidator())
 
 license_plate = reader.get_binary()
 
-characters = segmentator.get_characters_from_license_plate(license_plate)
+characters_matrix = segmentator.get_characters_from_license_plate(license_plate)
 
 saver = ImageSaver('../characters')
 model_mapper = ModelMapper()
 
 predictor = Predictor(model_mapper)
-predictor.clasify_characters(characters)
+predictor.clasify_characters(characters_matrix)
 characters = predictor.get_classified_characters()
 
 print(characters)
 
-# for character in characters:
-  # saver.save_as_train_data(character, model_mapper.train_image_width, model_mapper.train_image_height)
+# for each_character_matrix in characters_matrix :
+  # saver.save_as_train_data(each_character_matrix, model_mapper.train_image_width, model_mapper.train_image_height)
